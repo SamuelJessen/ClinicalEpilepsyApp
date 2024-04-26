838b4e23de37800d268920684fa4afd548b43269
SamuelJessen <201906916@post.au.dk>
2024-04-26 14:46:48 +0200

    Update patient model + add new migration to db

--- ClinicalEpilepsyApp.Domain/DBModels/Patient.cs
+++ ClinicalEpilepsyApp.Domain/DBModels/Patient.cs
@@ -4,7 +4,8 @@ public class Patient
 {
     public string Id { get; set; }
     public string Name { get; set; }
-    public int CSI { get; set; }
+    public int CSIThreshold { get; set; }
+    public int ModCSIThreshold { get; set; }
     public string Password { get; set; }
     public ICollection<EcgProcessedMeasurement> EcgProcessedMeasurements { get; set; }
     public ICollection<EcgAlarm> EcgAlarms { get; set; }
